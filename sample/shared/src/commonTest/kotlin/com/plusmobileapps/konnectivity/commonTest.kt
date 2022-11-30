a9ce673b030f4933ad2128b1c5905d97f12c5222
Andrew Steinmetz <andrew@plusmobileapps.com>
2022-11-29 21:58:20 -0800

    initial commit of kmm app in sample folder

---
+++ sample/shared/src/commonTest/kotlin/com/plusmobileapps/konnectivity/commonTest.kt
@@ -0,0 +1,12 @@
+package com.plusmobileapps.konnectivity
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class CommonGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Konnectivity"
+include(":sample:androidApp")
+include(":sample:shared")
